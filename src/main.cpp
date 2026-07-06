230563a1ea4fe4e467eac982da7af7cab98942ce
ali <159713281+cunkin375@users.noreply.github.com>
2026-07-05 20:58:36 -0400

    add basic top-left ray scanning

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,52 @@
 #include <iostream>
 
+#include "Util/Aliases.hpp"
+#include "Util/Log.hpp"
+
+#include "Color.hpp"
+#include "Ray.hpp"
+
+dColor RayToColor(const dRay &r) { return dColor{0, 0, 0}; }
+
 int main() {
-    std::cout << "Hello, world!\n";
+
+    // Image
+
+    auto image_width{400zu};
+    auto aspect_ratio{16.0 / 9.0};
+
+    auto image_height = std::size_t(image_width / aspect_ratio);
+    // image height must at least be 1
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (f64(image_width) / image_height);
+    auto camera_center = dPoint3{};
+
+    auto viewport_u = dVector3{viewport_width, 0, 0};
+    auto viewport_v = dVector3{0, -viewport_height, 0};
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_width;
+
+    auto viewport_upper_left = camera_center - dVector3{0, 0, focal_length} - viewport_u / 2 - viewport_v / 2;
+    auto pixel100_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (auto j{0zu}; j < image_height; j++) {
+        Log::Info("\rScanlines remaining: {}", (image_height - j));
+        for (auto i{0zu}; i < image_width; i++) {
+            auto pixel_center = pixel100_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            auto ray = dRay{camera_center, ray_direction};
+            auto pixel_color = RayToColor(ray);
+            WriteColor(std::cout, pixel_color);
+        }
+    }
+
+    Log::Info("Hello, world!");
 }
