d39abedb118c25a019740813dcdf3fdb09be91d6
ali <159713281+cunkin375@users.noreply.github.com>
2026-07-07 15:22:39 -0400

    make Old an actual namespace

--- src/Math/VectorOld.hpp
+++ src/Math/VectorOld.hpp
@@ -2,82 +2,86 @@
 #include <cstddef>
 
 // This is not used anymore but kept for reference
-namespace [[deprecated("Use Vector.hpp instead!!!")]] Old_Math {
+namespace Old {
 
-// Numbers are either floating points or integral types
-template <typename T>
-concept Number = std::floating_point<T> || std::integral<T>;
+namespace [[deprecated("Use Vector.hpp instead!!!")]] Math {
 
-// Forward Declaration
-template <std::size_t N, Number T>
-struct Vector;
+    // Numbers are either floating points or integral types
+    template <typename T>
+    concept Number = std::floating_point<T> || std::integral<T>;
 
-// 3D Vector of some Number Type
-// NOTE: Number is used to declare a template that contains only Number Types
-template <Number T>
-struct Vector<3zu, T> {
-    T x{}, y{}, z{};
+    // Forward Declaration
+    template <std::size_t N, Number T>
+    struct Vector;
 
-    // NOTE: constexpr allows for compile-time evaluation of constructors and functions
-    constexpr Vector() = default;
+    // 3D Vector of some Number Type
+    // NOTE: Number is used to declare a template that contains only Number Types
+    template <Number T>
+    struct Vector<3zu, T> {
+        T x{}, y{}, z{};
 
-    // Scalar copy
-    constexpr Vector(T scalar) : x(scalar), y(scalar), z(scalar) {}
+        // NOTE: constexpr allows for compile-time evaluation of constructors and functions
+        constexpr Vector() = default;
 
-    // Vector copy
-    constexpr Vector(T _x, T _y, T _z) : x(_x), y(_y), z(_z) {}
+        // Scalar copy
+        constexpr Vector(T scalar) : x(scalar), y(scalar), z(scalar) {}
 
-    // returns reference to modified vector type
-    constexpr Vector &operator+=(const Vector &right_vector) {
-        x += right_vector.x;
-        y += right_vector.y;
-        z += right_vector.z;
-        return *this; // <- do not change, this prevents undefined behavior
-    }
+        // Vector copy
+        constexpr Vector(T _x, T _y, T _z) : x(_x), y(_y), z(_z) {}
 
-    constexpr Vector &operator*=(const T right_number) {
-        x *= right_number;
-        y *= right_number;
-        z *= right_number;
-        return *this;
-    }
+        // returns reference to modified vector type
+        constexpr Vector &operator+=(const Vector &right_vector) {
+            x += right_vector.x;
+            y += right_vector.y;
+            z += right_vector.z;
+            return *this; // <- do not change, this prevents undefined behavior
+        }
 
-    // Hidden friend
-    // - Only evaluated if used
-    // - Ex of Argument-Dependeint Lookup (ADL)
-    friend constexpr Vector operator+(Vector left_vector, const Vector &right_vector) {
-        return left_vector += right_vector;
-    }
+        constexpr Vector &operator*=(const T right_number) {
+            x *= right_number;
+            y *= right_number;
+            z *= right_number;
+            return *this;
+        }
 
-    friend constexpr Vector operator*(Vector vector, T scalar) { return vector *= scalar; }
-};
+        // Hidden friend
+        // - Only evaluated if used
+        // - Ex of Argument-Dependeint Lookup (ADL)
+        friend constexpr Vector operator+(Vector left_vector, const Vector &right_vector) {
+            return left_vector += right_vector;
+        }
 
-// 2D Vector of some Number Type
-template <Number T>
-struct Vector<2zu, T> {
-    T x{}, y{};
+        friend constexpr Vector operator*(Vector vector, T scalar) { return vector *= scalar; }
+    };
 
-    constexpr Vector() = default;
-    constexpr Vector(T scalar) : x(scalar), y(scalar) {}
-    constexpr Vector(T _x, T _y) : x(_x), y(_y) {}
+    // 2D Vector of some Number Type
+    template <Number T>
+    struct Vector<2zu, T> {
+        T x{}, y{};
 
-    constexpr Vector &operator+=(const Vector &right) {
-        x += right.x;
-        y += right.y;
-        return *this;
-    }
+        constexpr Vector() = default;
+        constexpr Vector(T scalar) : x(scalar), y(scalar) {}
+        constexpr Vector(T _x, T _y) : x(_x), y(_y) {}
 
-    constexpr Vector &operator*=(const T right) {
-        x *= right;
-        y *= right;
-        return *this;
-    }
+        constexpr Vector &operator+=(const Vector &right) {
+            x += right.x;
+            y += right.y;
+            return *this;
+        }
 
-    friend constexpr Vector operator+(Vector left, const Vector &right) { return left += right; }
+        constexpr Vector &operator*=(const T right) {
+            x *= right;
+            y *= right;
+            return *this;
+        }
 
-    friend constexpr Vector operator*(Vector vector, T scalar) {
-        return Vector(vector.x * scalar, vector.y * scalar);
-    }
-};
+        friend constexpr Vector operator+(Vector left, const Vector &right) { return left += right; }
 
-} // namespace Old_Math
+        friend constexpr Vector operator*(Vector vector, T scalar) {
+            return Vector(vector.x * scalar, vector.y * scalar);
+        }
+    };
+
+} // namespace Math
+
+} // namespace Old
